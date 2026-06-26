f48b3d26a57890f0018e755d6f4fd3e45e869263
Sarthak Maurya <iitiansarthak2007@gmail.com>
2026-06-26 17:59:37 +0530

    Add files via upload

---
+++ basic code/VariableNamingRules.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+    //int xy = 57;
+    // cout<<xy<<endl;
+    // int x y = 57;
+    // cout<<x y<<endl;
+    // int 1x = 5; //ERROR
+    // cout<<iz<<endl;
+
+    // ! @ # $ % ^ & * ( ) { } [ ] - +
+    // = | \ / ? > < , . ~ ` ' ' : ;
+
+    //keyword - if for while
+    //int do = 6; error
+    // cout<<For;
+
+}
